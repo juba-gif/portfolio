aabfdb5aab3d14708a7187767f60859499785c50
Mojuba <oladuntoyemojuba@gmail.com>
2025-11-09 09:39:54 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import Greet from './Greeting.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
 <Greet />
-    <App />
+
   </StrictMode>,
 )
