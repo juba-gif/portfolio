963bbdae4ced9880fddb7138706b7399db8e0055
Mojuba <oladuntoyemojuba@gmail.com>
2025-11-09 09:44:59 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import Greet from './Greeting.tsx'
 
 createRoot(document.getElementById('root')!).render(
